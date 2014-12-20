78e5bb7bec0008c9a0c3dccca156283688d023b2
Chang Liu <cliu712@aucklanduni.ac.nz>
2014-12-20 13:48:29 +1300

    Add /api/admin apis

--- api/admin.js
+++ api/admin.js
@@ -1,14 +1,71 @@
 exports.route = function(app) {
-    app.post('/new', function(req, res){
+    var db = require('redis').createClient();
+
+    app.post('/new', function(req, res) {
 	var name = req.query.name;
 	if (!name) {
 	    res.status(400).send('Need admin name')
 	    return
 	}
-	res.send('admin name' + name);
+	db.sismember('admins', name, function(err, reply) {
+	    if (reply == 1) {
+		res.status(400).send('Admin ' + name + ' exists')
+		return
+	    }
+
+	    db.sadd('admins', name, function(err, reply) {
+		if (reply == 1) {
+		    res.send('Admin ' + name + ' added')
+		} else {
+		    res.send('Failed to add admin ' + name + ': ' + err)
+		}
+	    })
+	})
     })
 
-    app.post('/get', function(req, res){
-	res.send('admin get');
+    app.post('/del', function(req, res) {
+	var name = req.query.name;
+	if (!name) {
+	    res.status(400).send('Need admin name')
+	    return
+	}
+	db.sismember('admins', name, function(err, reply) {
+	    if (reply == 0) {
+		res.status(400).send('Admin ' + name + ' does not exist')
+		return
+	    }
+
+	    db.srem('admins', name, function(err, reply) {
+		if (reply == 1) {
+		    res.send('Admin ' + name + ' deleted')
+		} else {
+		    res.send('Failed to delete admin ' + name + ': ' + err)
+		}
+	    })
+	})
+    })
+
+    app.post('/get', function(req, res) {
+	function write_admin_info(name, admins, i, n) {
+	    db.smembers(name+':courses', function(err, reply) {
+		var courses = reply ? reply : []
+		admins[i] = { 'name' : name, 'courses' : courses }
+		if (i == n-1) {
+		    res.send(JSON.stringify(admins))
+		}
+	    })
+	}
+
+	var admins = [];
+
+	db.smembers('admins', function(err, reply) {
+	    if (err) {
+		res.send('Db access failed: ' + err)
+		return
+	    }
+	    for (var i = 0; i < reply.length; i++) {
+		write_admin_info(reply[i], admins, i, reply.length)
+	    }
+	})
     })
 }
