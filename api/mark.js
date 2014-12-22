88b0bdfdf3148c8d97f24d07041b116464c057f9
Chang Liu <cliu712@aucklanduni.ac.nz>
2014-12-22 18:41:58 +1300

    Start worling on frontend!

--- api/mark.js
+++ api/mark.js
@@ -1,9 +1,3 @@
-exports.route = function(app) {
-    app.post('/new', function(req, res){
-	res.send('admin new')
-    })
-
-    app.post('/get', function(req, res){
-	res.send('admin get');
-    })
+exports.handler = function(req, res) {
+    res.send('mark')
 }
--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 var express = require('express');
-require('express-namespace');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var api = require('./api/api');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -23,38 +16,22 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./api/auth').middleware);
-app.use(api);
+app.use(require('./api/api'));
+
+app.use(require('./redirect'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    var err = new Error('Not found')
+    err.status = 404
+    next(err)
+})
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// display errors
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+    res.status(err.status)
+    res.send('<!DOCTYPE html><html><body>'
+	     + err.message + '</body></html>')
+})
 
-
-module.exports = app;
+module.exports = app
